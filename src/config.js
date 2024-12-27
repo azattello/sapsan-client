d0d007c45add95d0014be4fc4c63b8e15de7c1f9
Abdykali Azat <azat.abdykalli@gmail.com>
2024-12-27 21:50:02 +0500

    first commit

---
+++ src/config.js
@@ -0,0 +1,12 @@
+// config.js
+const config = {
+    // apiUrl: 'http://localhost:3001',
+    apiUrl: 'https://sapsancargo.kz:3001',
+   
+    // nameCargo: 'TURKESTAN KARGO'
+    nameCargo: 'SAPSAN CARGO'
+  };
+  
+  
+export default config;
+  
